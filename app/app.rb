171408dc79cb4d393d87e18c8a14ceca18c6f47e
Yuhei Okazaki <okazaki@fusic.co.jp>
2019-02-10 23:35:45 +0900

    modify template.yaml

--- app/app.rb
+++ app/app.rb
@@ -9,17 +9,11 @@ def gyazo_upload(event:, context:)
   body = Base64.decode64(body) if event['isBase64Encoded']
   key = SecureRandom.urlsafe_base64
   object = Aws::S3::Resource
-             .new(region:'ap-northeast-1')
+             .new(region:ENV['REGION'])
              .bucket(ENV['BUCKET_NAME'])
              .put_object({ key: "#{key}.png", content_type: 'image/png', body: body })
   {
     statusCode: 200,
-    body: {
-      image_id: key,
-      permalink_url: object.public_url,
-      thumb_url: object.public_url,
-      url: object.public_url,
-      type: 'png'
-    }.to_json
+    body: object.public_url
   }
 end
